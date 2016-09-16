3f38752eb861acd1fa14b9ee079c8b1deccaefe2
Owen Lilly <owelilly@gmail.com>
2016-09-16 11:19:33 -0500

    Added ObjectID to exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     RxMongo: require('./lib/RxMongo.js'),
     RxCursor: require('./lib/RxCursor.js'),
-    RxCollection: require('./lib/RxCollection.js')
+    RxCollection: require('./lib/RxCollection.js'),
+    ObjectID: require('mongodb').ObjectID
 };
